75a63ccc8268bc60e241b29142deb85d36ea75c2
Joel Seng <joel29dec@gmail.com>
2019-07-08 16:36:58 -0500

    backend is connected

--- flow-frontend/index.js
+++ flow-frontend/index.js
@@ -5,16 +5,17 @@
   const quadrantFour = document.querySelector('#Q4');
 
   const Q1ToDo = document.querySelector('#Q1-todo');
-  const Q1Items = JSON.parse(localStorage.getItem('Q1Items')) || [];
+
+  let Q1Items;
 
   const Q2ToDo = document.querySelector('#Q2-todo');
-  const Q2Items = JSON.parse(localStorage.getItem('Q2Items')) || [];
+  let Q2Items;
   
   const Q3ToDo = document.querySelector('#Q3-todo');
-  const Q3Items = JSON.parse(localStorage.getItem('Q3Items')) || [];
+  let Q3Items = JSON.parse(localStorage.getItem('Q3Items')) || [];
 
   const Q4ToDo = document.querySelector('#Q4-todo');
-  const Q4Items = JSON.parse(localStorage.getItem('Q4Items')) || [];
+  let Q4Items = JSON.parse(localStorage.getItem('Q4Items')) || [];
 
 
   function addItem(e, itemList, quadrant) {
@@ -64,80 +65,32 @@
 
   Q1ToDo.addEventListener('click', toggleDone);
 
-  populateList(Q1Items, Q1ToDo);
-  populateList(Q2Items, Q2ToDo);
-  populateList(Q3Items, Q3ToDo);
-  populateList(Q4Items, Q4ToDo);
-
-
-  // fetch('http://localhost:3000/items').then(resp => resp.json()).then(data => console.log(data))
-
-
-  // const quadrantOne = document.querySelector('#Q1');
-  // const quadrantTwo = document.querySelector('#Q2');
-  // const quadrantThree = document.querySelector('#Q3');
-  // const quadrantFour = document.querySelector('#Q4');
-
-  // const Q1ToDo = document.querySelector('#Q1-todo');
-  // const Q1Items = JSON.parse(localStorage.getItem('Q1Items')) || [];
-
-  // const Q2ToDo = document.querySelector('#Q2-todo');
-  // const Q2Items = JSON.parse(localStorage.getItem('Q2Items')) || [];
   
-  // const Q3ToDo = document.querySelector('#Q3-todo');
-  // const Q3Items = JSON.parse(localStorage.getItem('Q3Items')) || [];
-
-  // const Q4ToDo = document.querySelector('#Q4-todo');
-  // const Q4Items = JSON.parse(localStorage.getItem('Q4Items')) || [];
-
-  // function addItem(e, itemList, quadrant) {
-  //   e.preventDefault();
-  //   const text = (e.target.querySelector('[name=item]')).value;
-  //   const item = {
-  //     text,
-  //     done: false
-  //   };
-  //   itemList.push(item);
-  //   populateList(itemList, quadrant);
-  //   localStorage.setItem('itemList', JSON.stringify(itemList));
-  //   e.target.reset();
-  // }
+  fetchListItems();
 
 
+function fetchListItems(){
+  fetch('http://localhost:3000/items').then(resp => resp.json()).then(data => assignList(data))
+}
 
-  // function removeAllItems(){
-  //   localStorage.removeItem('Q1Items');
-  //   Q1ToDo.innerHTML = ''
-  // }
+function assignList(data){
+  
+    Q1Items = data.filter(item => item.category_id == 1) || JSON.parse(localStorage.getItem('Q1Items')) || [];
 
-  // function populateList(todo = [], todoList) {
-  //   todoList.innerHTML = todo.map((item, i) => {
-  //     return `
-  //       <li>
-  //         <input type="checkbox" data-index=${i} id="item${i}" ${item.done ? 'checked' : ''} />
-  //         <label for="item${i}">${item.text}</label>
-  //       </li>
-  //     `;
-  //   }).join('');
-  // }
+    Q2Items = data.filter(item => item.category_id == 2) || JSON.parse(localStorage.getItem('Q2Items')) || [];
 
-  // function toggleDone(e) {
-  //   if (!e.target.matches('input')) return; 
-  //   const el = e.target;
-  //   const index = el.dataset.index;
-  //   Q1Items[index].done = !Q1Items[index].done;
-  //   localStorage.setItem('Q1Items', JSON.stringify(Q1Items));
-  //   populateList(Q1Items, Q1ToDo);
-  // }
+    Q3Items = data.filter(item => item.category_id == 3) || JSON.parse(localStorage.getItem('Q3Items')) || [];
 
-  // quadrantOne.addEventListener('submit', (e) => {addItem(e, Q1Items, Q1ToDo)});
-  // quadrantTwo.addEventListener('submit', (e) => {addItem(e, Q2Items, Q2ToDo)});
-  // quadrantThree.addEventListener('submit', (e) => {addItem(e, Q3Items, Q3ToDo)});
-  // quadrantFour.addEventListener('submit', (e) => {addItem(e, Q4Items, Q4ToDo)});
 
-  // Q1ToDo.addEventListener('click', toggleDone);
+    Q4Items = data.filter(item => item.category_id == 4) || JSON.parse(localStorage.getItem('Q4Items')) || [];
 
-  // populateList(Q1Items, Q1ToDo);
-  // populateList(Q2Items, Q2ToDo);
-  // populateList(Q3Items, Q3ToDo);
-  // populateList(Q4Items, Q3ToDo);
+    populateList(Q1Items, Q1ToDo);
+    populateList(Q2Items, Q2ToDo);
+    populateList(Q3Items, Q3ToDo);
+    populateList(Q4Items, Q4ToDo);
+    
+    console.log(Q1Items)
+    console.log(Q2Items)
+    console.log(Q3Items)
+    console.log(Q4Items)
+}
