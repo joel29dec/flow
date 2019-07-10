448ba59ef830dc0f1eb1b16125310b0498cbb294
Joel Seng <joel29dec@gmail.com>
2019-07-10 09:44:09 -0500

    edit button event handler works

---
+++ flow-frontend/dictionary.js
@@ -0,0 +1,41 @@
+//element for Quadrant forms input section
+const quadrantOne = document.querySelector('#Q1');
+const quadrantTwo = document.querySelector('#Q2');
+const quadrantThree = document.querySelector('#Q3');
+const quadrantFour = document.querySelector('#Q4');
+const body = document.querySelector('body')
+
+//ul of Quadrant items
+const Q1ToDo = document.querySelector('#Q1-todo');
+let Q1Items;
+const Q2ToDo = document.querySelector('#Q2-todo');
+let Q2Items;
+const Q3ToDo = document.querySelector('#Q3-todo');
+let Q3Items = JSON.parse(localStorage.getItem('Q3Items')) || [];
+const Q4ToDo = document.querySelector('#Q4-todo');
+let Q4Items = JSON.parse(localStorage.getItem('Q4Items')) || [];
+
+const Q1EditBtn = document.querySelector('#Q1-edit-btn');
+
+//event listeners for edit
+Q1EditBtn.addEventListener('click', e => editForm(e, Q1Items, Q1ToDo))
+
+
+
+//event listeners for submit
+quadrantOne.addEventListener('submit', e => addItem(e, Q1Items, Q1ToDo));
+quadrantTwo.addEventListener('submit', e =>  addItem(e, Q2Items, Q2ToDo));
+quadrantThree.addEventListener('submit', e => addItem(e, Q3Items, Q3ToDo));
+quadrantFour.addEventListener('submit', e => addItem(e, Q4Items, Q4ToDo));
+
+//event listeners for checkbox
+Q1ToDo.addEventListener('click', e => toggleDone(e, Q1Items, Q1ToDo))
+Q2ToDo.addEventListener('click', e => toggleDone(e, Q2Items, Q2ToDo));
+Q3ToDo.addEventListener('click', e => toggleDone(e, Q3Items, Q3ToDo));
+Q4ToDo.addEventListener('click', e => toggleDone(e, Q4Items, Q4ToDo));
+
+//event listeners for delete
+Q1ToDo.addEventListener('click', e => {deleteItem(e, Q1ToDo); Q1Items = Q1Items.filter( item =>  item.id != e.target.dataset.id ); console.log(Q1Items)})
+Q2ToDo.addEventListener('click', e => {deleteItem(e, Q2ToDo); Q2Items = Q2Items.filter( item =>  item.id != e.target.dataset.id ); console.log(Q2Items)})
+Q3ToDo.addEventListener('click', e => {deleteItem(e, Q3ToDo); Q3Items = Q3Items.filter( item =>  item.id != e.target.dataset.id ); console.log(Q3Items)})
+Q4ToDo.addEventListener('click', e => {deleteItem(e, Q4ToDo); Q4Items = Q4Items.filter( item =>  item.id != e.target.dataset.id ); console.log(Q4Items)})
--- flow-frontend/index.js
+++ flow-frontend/index.js
@@ -1,20 +1,3 @@
-//element for Quadrant forms input section
-const quadrantOne = document.querySelector('#Q1');
-const quadrantTwo = document.querySelector('#Q2');
-const quadrantThree = document.querySelector('#Q3');
-const quadrantFour = document.querySelector('#Q4');
-const body = document.querySelector('body')
-
-//ul of Quadrant items
-const Q1ToDo = document.querySelector('#Q1-todo');
-let Q1Items;
-const Q2ToDo = document.querySelector('#Q2-todo');
-let Q2Items;
-const Q3ToDo = document.querySelector('#Q3-todo');
-let Q3Items = JSON.parse(localStorage.getItem('Q3Items')) || [];
-const Q4ToDo = document.querySelector('#Q4-todo');
-let Q4Items = JSON.parse(localStorage.getItem('Q4Items')) || [];
-
 //render li elements
 function populateList(todo = [], todoList) {
   todoList.innerHTML = todo.map((item, i) => {
@@ -23,50 +6,22 @@ function populateList(todo = [], todoList) {
         <input type="checkbox" data-id=${item.id} id="item${i}" ${item.done ? 'checked' : ''} />
         <label for="item${i}">${item.text}</label>
         <button type="button" class="btn btn-default btn-sm" data-id=${item.id}>
-        <span class="glyphicon glyphicon-trash"></span> Trash 
+        <span class="glyphicon glyphicon-trash"></span> 
       </button>
       </li>
     `;
   }).join('');
 }
 
-//event listeners for submit
-quadrantOne.addEventListener('submit', e => addItem(e, Q1Items, Q1ToDo));
-quadrantTwo.addEventListener('submit', e =>  addItem(e, Q2Items, Q2ToDo));
-quadrantThree.addEventListener('submit', e => addItem(e, Q3Items, Q3ToDo));
-quadrantFour.addEventListener('submit', e => addItem(e, Q4Items, Q4ToDo));
-
-//event listeners for checkbox
-Q1ToDo.addEventListener('click', e => toggleDone(e, Q1Items, Q1ToDo))
-Q2ToDo.addEventListener('click', e => toggleDone(e, Q2Items, Q2ToDo));
-Q3ToDo.addEventListener('click', e => toggleDone(e, Q3Items, Q3ToDo));
-Q4ToDo.addEventListener('click', e => toggleDone(e, Q4Items, Q4ToDo));
-
-//event listeners for delete
-Q1ToDo.addEventListener('click', e => {deleteItem(e, Q1ToDo); Q1Items = Q1Items.filter( item =>  item.id != e.target.dataset.id ); console.log(Q1Items)})
-Q2ToDo.addEventListener('click', e => {deleteItem(e, Q2ToDo); Q2Items = Q2Items.filter( item =>  item.id != e.target.dataset.id ); console.log(Q2Items)})
-Q3ToDo.addEventListener('click', e => {deleteItem(e, Q3ToDo); Q3Items = Q3Items.filter( item =>  item.id != e.target.dataset.id ); console.log(Q3Items)})
-Q4ToDo.addEventListener('click', e => {deleteItem(e, Q4ToDo); Q4Items = Q4Items.filter( item =>  item.id != e.target.dataset.id ); console.log(Q4Items)})
-
-// //event listener for edit
-
-// Q1ToDo.addEventListener('dblclick', e => editItem(e, Q1Items, Q1ToDo))
-
-
-// function editItem(e, itemList, quadrant){
-//   console.log(e.target)
-// }
 
 
 
 
-
-
-
-
-
-
-
+function editForm(e, itemList, quadrant){
+  console.log(e.target)
+  console.log(itemList)
+  console.log(quadrant)
+}
 
 
 //event handler callbacks
